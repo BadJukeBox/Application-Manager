7a99122c19b32df632dca04a2bf0fbac67bf2551
Justin <justinwilkowski@yahoo.com>
2017-02-13 23:38:18 -0800

    ALL OF IT
    Main Window, Input Form

---
+++ App_Manager/App_Manager/InputForm.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace App_Manager
+{
+    /// <summary>
+    /// Interaction logic for InputForm.xaml
+    /// </summary>
+    public partial class InputForm : Window
+    {
+        Form_Handler form;
+        public InputForm()
+        {
+            form = new Form_Handler(this);
+            InitializeComponent();
+        }
+
+        public string Company_Name
+        {
+            get { return Company.Text; }
+        }
+        public string Position_Name
+        {
+            get { return Position.Text; }
+        }
+        public string Date_Text
+        {
+            get { return Date_of_App.Text; }
+        }
+        public string Req_ID_Text
+        {
+            get { return Requisition_ID.Text; }
+        }
+        public string Additional_Text
+        {
+            get { return Addition_Info.Text; }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            form.saveForm();
+        }
+    }
+}
